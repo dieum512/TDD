6bae28b6edc19b5458ec9dde90eecf4c086f21fc
dieum512 <akonkwadieum222@gmail.com>
2023-06-17 02:07:48 +0200

    Update the test file

--- tests/solver_spec.rb
+++ tests/solver_spec.rb
@@ -6,7 +6,7 @@ describe Solver do
   describe '#factorial' do
     it 'throw an error if input is negative number' do
       expect { @solver.factorial(-1) }.to raise_error(
-        'input can not be negative',
+        'Input must be positive',
         RuntimeError
       )
     end
@@ -20,8 +20,26 @@ describe Solver do
     end
   end
 
-  describe '#reverse_string' do
-    it 'return reversed string' do
-      expect(@solver.reverse_string('hello')).to eql 'olleh'
+  describe '#reverse' do
+    it 'returns a reversed string' do
+      expect(@solver.reverse('hello')).to eql 'olleh'
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'returns fizz for the input divisible by 3' do
+      expect(@solver.fizzbuzz(9)).to eql 'fizz'
+    end
+    it 'returns buzz for the input divisible by 5' do
+      expect(@solver.fizzbuzz(10)).to eql 'buzz'
+    end
+    it 'returns fizzbuzz for the input divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
+    end
+
+    it 'returns number as string for any other number' do
+      expect(@solver.fizzbuzz(7)).to eql '7'
+      expect(@solver.fizzbuzz(7).class).to eql String
+    end
+  end
+end
